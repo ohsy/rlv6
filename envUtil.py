8afe15822f9b180a4e012ad2496874bc496d8ce2
yoh <neocella@naver.com>
2023-05-23 22:16:29 +0900

    files tidied

--- envUtil.py
+++ envUtil.py
@@ -1,10 +1,10 @@
 
 import numpy as np
-from NodeCode import NodeCode
+from NodeCoder import NodeCoder
 
 class EnvUtil:
-    observCoder : NodeCode
-    actionCoder : NodeCode
+    observCoder : NodeCoder
+    actionCoder : NodeCoder
 
     @classmethod
     def experienceFrom(cls, observFrEnv, actionToEnv, reward, next_observFrEnv, done, npDtype):
@@ -19,4 +19,16 @@ class EnvUtil:
                 np.array([done], dtype=npDtype)         # bool to ndarray of dtype
         )
         return ex
+
+
+class EnvUtil_CartPole_v1(EnvUtil):
+    observCoder = NodeCoder(nNodes=[1,1,1,1],
+                           low=[-4.8, np.finfo(np.float32).min / 2, -0.418, np.finfo(np.float32).min / 2],
+                           high=[4.8, np.finfo(np.float32).max / 2, 0.418, np.finfo(np.float32).max / 2], scaleshift=None)
+    actionCoder = NodeCoder(nNodes=[2], possibles=[[0,1]], scaleshift=None, isDecodedScalar=True)
+
+
+class EnvUtil_Pendulum_v1(EnvUtil):
+    observCoder = NodeCoder(nNodes=[1,1,1], low=[-1, -1, -8], high=[1,1,8], scaleshift=None)
+    actionCoder = NodeCoder(nNodes=[1], low=[-2], high=[2], scaleshift=None)
 
