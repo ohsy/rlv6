747b5aad7eb8d3e61454414d3df9945d8b32577e
soh <neocella@gmail.com>
2023-08-07 15:30:13 +0900

    analyzer_daiso.py fixed

--- analyzer_daiso.py
+++ analyzer_daiso.py
@@ -6,7 +6,6 @@ class Analyzer_daiso(Analyzer):
     def afterTimestep(self, reward, info):
         super().afterTimestep(reward, info)
         tf.summary.scalar("reward", reward, step=self.stepCnt)
-        if self.envName in ["DaisoSokcho", "DaisoSokcho_discrete"]:
         with self.summaryWriter.as_default():
             tf.summary.scalar("cost_term", info['reward_terms']["cost_term"], step=self.stepCnt)
             tf.summary.scalar("temperature_term", info['reward_terms']["temperature_term"], step=self.stepCnt)
